461c5aa3e05a2d89eb3b42a5fe49f759294fad1b
pratiksha.jain <pratikshaj2650@gmail.com>
2026-02-02 21:41:57 +0530

    Base push

---
+++ src/test/java/com/pj/notificationFSSP/NotificationFsspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pj.notificationFSSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationFsspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
